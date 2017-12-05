961c7851dbecb0827ef40383c09734a395ccac7a
tianyi <tianyi@gshopper.com>
2017-12-05 16:44:09 +0800

    Initial commit

---
+++ src/main/java/com/example/video/web/VideoWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.video.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoWebApplication.class, args);
+	}
+}
